e559fec006cafe65de66d6bb6bc854014e4eb866
Puru80 <puru.agar99@gmail.com>
2022-05-07 16:47:59 +0530

    Initial Commit

--- src/main/java/com/puru80/flashmonkecommerce/product/Product.java
+++ src/main/java/com/puru80/flashmonkecommerce/product/Product.java
@@ -3,12 +3,25 @@ package com.puru80.flashmonkecommerce.product;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Data
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "productId", nullable = false)
+    private Long productId;
+
+    @NotNull(message = "Product name cannot be null")
+    @Column(name = "productName")
+    @Basic(optional = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
 }
